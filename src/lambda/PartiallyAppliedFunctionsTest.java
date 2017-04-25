32a31b161a5d61feb87c71e73c1fb2b4882e98a8
atul-bharti <atul.bharti@gmail.com>
2017-04-25 15:28:09 +0530

    Add files via upload

---
+++ src/lambda/PartiallyAppliedFunctionsTest.java
@@ -0,0 +1,38 @@
+package lambda;
+
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import org.junit.Test;
+
+public class PartiallyAppliedFunctionsTest {
+
+  Function<Integer,Function<Integer,Function<BinaryOperator<Integer>,Integer>>> 
+    someComputation = i1 -> i2 -> f -> f.apply(i1,i2);
+
+  BinaryOperator<Integer> mult = (i,j) -> i * j;
+  BinaryOperator<Integer> divide = (i,j) -> i / j;
+  BinaryOperator<Integer> sumSquares = (i,j) -> (i*i) + (j*j);
+
+  int first = 10;
+  int second = 5;
+
+  Function<Integer,Function<BinaryOperator<Integer>,Integer>> partial1 = someComputation.apply(first);
+  Function<BinaryOperator<Integer>,Integer> partial2 = partial1.apply(second);
+
+
+  @Test
+  public void test_multiplication(){
+     // assertThat(partial2.apply(mult),is(50));
+  }
+
+  @Test
+  public void test_divide(){
+     // assertThat(partial2.apply(divide),is(2));
+  }
+
+  @Test
+  public void test_sum_squares(){
+      //assertThat(partial2.apply(sumSquares),is(125));
+  }
+
+}
